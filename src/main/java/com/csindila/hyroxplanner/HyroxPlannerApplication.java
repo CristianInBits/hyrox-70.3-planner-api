a11148a311d7b822a6085488c64dddedf6b2d4b8
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-09-23 14:07:19 +0200

    chore: proyecto base Spring Boot + Flyway + Compose (V1 migración)

---
+++ src/main/java/com/csindila/hyroxplanner/HyroxPlannerApplication.java
@@ -0,0 +1,11 @@
+package com.csindila.hyroxplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyroxPlannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HyroxPlannerApplication.class, args);
+    }
+}
